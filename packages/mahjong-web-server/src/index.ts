53e6b33c13b694530baabfce47e395d2cd684d48
Rouh Asami <roaming.new@gmail.com>
2025-07-20 08:21:07 +0900

    更新

--- packages/mahjong-web-server/src/index.ts
+++ packages/mahjong-web-server/src/index.ts
@@ -16,6 +16,7 @@ const io = new Server(server, {
       ? ['https://rouhjp.github.io']
       : ["http://localhost:5173", "http://127.0.0.1:5173"], 
     methods: ["GET", "POST"],
+    credentials: true,
   }
 });
 
